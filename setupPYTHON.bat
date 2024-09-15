44d5254adf0d5711b1e007c606e72eedc79ca9eb
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-15 22:11:06 +0200

    Update setupPYTHON.bat

--- setupPYTHON.bat
+++ setupPYTHON.bat
@@ -3,8 +3,10 @@ pip install -r "%~dp0requirements.txt"
 set "directory=%~dp0"
 if "%directory:~-1%"=="\" set "directory=%directory:~0,-1%"
 set "PATH=%PATH%;%directory%"
-echo @echo off > "%directory%ccdir.bat"
-echo python "%directory%ccdir.py" %%* >> "%directory%ccdir.bat"
-del "%directory%setupPY.bat"
-del "%directory%setupPYTHON.bat"
+(
+    echo @echo off
+    echo python "%directory%ccdir.py" %%*
+) > "%directory%ccdir.bat"
+del "%~dp0setupPY.bat"
+del "%~dp0setupPYTHON.bat"
 setx PATH "%PATH%;%directory%"
