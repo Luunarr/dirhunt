e8f0997690cc611693d245584f684a6808634abe
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-15 21:59:06 +0200

    Delete setup.bat

--- setup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pip install -r "%~dp0requirements.txt"
-set "directory=%~dp0"
-if "%directory:~-1%"=="\" set "directory=%directory:~0,-1%"
-setx PATH "%PATH%;%directory%"
-echo @echo off > "%directory%ccdir.bat"
-echo py "%directory%ccdir.py" %%* >> "%directory%ccdir.bat"
