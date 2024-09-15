735e34948206f2f7d2919e549c9aae15ba929b38
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-15 22:08:06 +0200

    Add files via upload

--- setupPYTHON.bat
+++ setupPYTHON.bat
@@ -2,8 +2,9 @@
 pip install -r "%~dp0requirements.txt"
 set "directory=%~dp0"
 if "%directory:~-1%"=="\" set "directory=%directory:~0,-1%"
-setx PATH "%PATH%;%directory%"
+set "PATH=%PATH%;%directory%"
 echo @echo off > "%directory%ccdir.bat"
 echo python "%directory%ccdir.py" %%* >> "%directory%ccdir.bat"
-del setupPY.bat
-del setupPYTHON.bat
+del "%directory%setupPY.bat"
+del "%directory%setupPYTHON.bat"
+setx PATH "%PATH%;%directory%"
