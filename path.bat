6c8f1e7973e37d26980df4fc21d8150cb6490b75
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-18 12:52:51 +0200

    Update path.bat

--- path.bat
+++ path.bat
@@ -1,1 +1,6 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: dirhunt/path.bat  (https://github.com/Luunarr/dirhunt) ::
+:: by Lunar          (https://github.com/Luunarr)         ::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 cd %USERPROFILE%
