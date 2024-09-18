f776b9cfde7c4233d811e2ec8d0ba0e3b5738a33
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-18 13:11:28 +0200

    Update path.bat

--- path.bat
+++ path.bat
@@ -3,6 +3,7 @@
 :: by Lunar          (https://github.com/Luunarr)         ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+@echo off
 echo python %~dp0\dirhunt.py > dirhunt.bat
 setx /M path "%path%;%~dp0"
 cls
