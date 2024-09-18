e32e5922b56aee05f2593f2c64e9da36582f2f8c
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-18 14:13:45 +0200

    Update path.bat

--- path.bat
+++ path.bat
@@ -4,11 +4,14 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 @echo off
+setlocal
 set "SCRIPT_DIR=%~dp0Scripts"
 if not exist "%SCRIPT_DIR%" mkdir "%SCRIPT_DIR%"
 echo @echo off > "%SCRIPT_DIR%\dirhunt.bat"
 echo python "%~dp0\dirhunt.py" %%* >> "%SCRIPT_DIR%\dirhunt.bat"
 move "%~f0" "%SCRIPT_DIR%\"
-setx /M path "%PATH%;%SCRIPT_DIR%"
+set "NEW_PATH=%PATH%;%SCRIPT_DIR%"
+setx /M PATH "%NEW_PATH%"
+echo %NEW_PATH% > "%SCRIPT_DIR%\path_check.txt"
 cls
 pause
