e795e1b6f39a75bdf091c9b397318615fc6ef60c
Lunar <gwkgxkgclydktflhxkrflgg@gmail.com>
2024-09-21 22:56:46 +0200

    Update dirhunt.py

--- dirhunt.py
+++ dirhunt.py
@@ -66,10 +66,10 @@ ver = f"{reset}{bright}{lightyellow}[{white}dirhunt{gray}#{white}1.2{lightyellow
 ##############################################################
 
 asciiart = [
-f"{bright}{white}   ___      _             _  _                     _    {reset}",
-f"{bright}{white}  |   \    (_)      _ _  | || |   _  _    _ _     | |_  {reset}",
-f"{bright}{white}  | |) |   | |     | '_| | __ |  | {cyan}+{white}| |  | ' \    |  _|   {ver}{reset}",
-f"{bright}{white}  |___/   _|_|_   _|_|_  |_||_|   \_,_|  |_||_|   _\__|   {white}https://github.com/Luunarr/dirhunt{reset}",
+f"{bright}   {cyan}___{white}      _             {cyan}_  _{white}                     _    {reset}",
+f"{bright}  {cyan}|   \ {white}   (_)      _ _  {cyan}| || |{white}   _  _    _ _     | |_  {reset}",
+f"{bright}  {cyan}| |) |{white}   | |     | '_| {cyan}| __ |{white}  | {cyan}+{white}| |  | ' \    |  _|   {ver}{reset}",
+f"{bright}  {cyan}|___/{white}   _|_|_   _|_|_  {cyan}|_||_|{white}   \_,_|  |_||_|   _\__|   {white}https://github.com/Luunarr/dirhunt{reset}",
 f'{bright}{white}_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|{reset}',
 f"""{bright}{white}"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'{reset}""",
 f""
